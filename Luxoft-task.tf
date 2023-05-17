f145af93ecd0b12f3af19ec558cc5b8b26e04c47
beatplugg <nonameqwerty87@gmail.com>
2023-05-17 13:46:51 +0300

    sg, dockerfiles, provisioning

--- Luxoft-task.tf
+++ Luxoft-task.tf
@@ -4,7 +4,84 @@ provider "aws" {
 	region = "eu-central-1"
 }
 
-resource "aws_instance" "terraform-task" {
+resource "aws_key_pair" "terraform-task-key" {
+	key_name = "terraform_key"
+	public_key = file("./terraform.pub") 
+}
+
+resource "aws_security_group" "terraform-task-sg" {
+  name        = "terraform-task-sg"
+  description = "Opening ingress ports for ssh, https, http, grafana, prometheus"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  } 
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "terraform-task-instance" {
 	ami = "ami-0ec7f9846da6b0f61"
 	instance_type = "t2.micro"
+	key_name = aws_key_pair.terraform-task-key.key_name
+	vpc_security_group_ids = [aws_security_group.terraform-task-sg.id]
+}
+
+provisioner "file" {
+    source      = "./grafana/Dockerfile"
+    destination = "/home/ubuntu/dockerfiles/grafana"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "docker build -t grafana /home/ubuntu/dockerfiles/grafana",
+      "docker run -d grafana"
+    ]
+  }
+
+  provisioner "file" {
+
+  source = "./prometheus/Dockerfile"
+  destination = "/home/ubuntu/dockefriles/prometheus"
+  }
+
+  provisioner "remote-exec" {
+  inline [
+  "docker build -t prometheus /home/ubuntu/dockerfiles/prometheus
+  "docker run -d prometheus"
+  	]
+  }
+
+
+resource "aws_eip" "terraform-task-eip" {
+	instance = aws_instance.terraform-task-instance.id	
 }
