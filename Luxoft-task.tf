4db9139abe96a36a29336a706ff81e22160ca743
beatplugg <nonameqwerty87@gmail.com>
2023-05-17 11:46:06 +0300

    provider & resource

---
+++ Luxoft-task.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+	access_key = "xxx"
+	secret_key = "xxx"
+	region = eu-central-1
+}
+
+resource "aws_instance" "terraform-task" {
+	ami = "ami-0ec7f9846da6b0f61"
+	instance_type = "t2.micro"
+}
