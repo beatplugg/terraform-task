3f6ce0ea0b644f009b0a35623ff36b42aa5eb829
beatplugg <nonameqwerty87@gmail.com>
2023-05-17 15:14:20 +0300

    .tf

--- Luxoft-task.tf
+++ Luxoft-task.tf
@@ -1,52 +1,52 @@
-provider "aws" {
+provider = "aws" {
 	access_key = "xxx"
 	secret_key = "xxx"
-	region = "eu-central-1"
+	region "eu-central-1"
 }
 
 resource "aws_key_pair" "terraform-task-key" {
-	key_name = "terraform_key"
-	public_key = file("./terraform.pub") 
+	key_name = "terraform-key"
+	public_key = file("./terraform.pub")
 }
 
 resource "aws_security_group" "terraform-task-sg" {
-  name        = "terraform-task-sg"
-  description = "Opening ingress ports for ssh, https, http, grafana, prometheus"
+	name = "terraform-task-sg"
+	description = "Opening ingress ports for ssh, https, http, grafana, prometheus"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	ingress {
+	from_port = 22
+	to_port = 22
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  } 
+	ingress {
+	from_port = 443
+	to_port = 443
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	ingress {
+	from_port = 80
+	to_port = 80
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
 
-  ingress {
-    from_port   = 3000
-    to_port     = 3000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	ingress {
+	from_port = 3000
+	to_port = 3000
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
 
-  ingress {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	ingress {
+	from_port = 9090
+	to_port = 9090
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
 resource "aws_instance" "terraform-task-instance" {
@@ -60,51 +60,48 @@ resource "aws_eip" "terraform-task-eip" {
 	instance = aws_instance.terraform-task-instance.id	
 }
 
-resource "null_resource" "example" {
-  depends_on = [aws_eip.example]
+resource "null_resource" "terraform-task-null" {
+	depends_on = [aws_eip.terraform-task-eip]
+}
 
- provisioner "remote-exec" {
-    inline = [
-
-    "sudo apt-get update",
-    "sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
-    "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
-    "echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
-    "sudo apt-get update",
-    "sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-    "sudo usermod -aG docker ${USER}"
-	"sudo systemctl enable docker",
-    "sudo systemctl start docker",
-
+provisioner "remote-exec" {
+  	inline = [
+   	"sudo apt-get update",
+   	"sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common",
+   	"curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg",
+   	"echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+   	"sudo apt-get update",    
+   	"sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
+   	"sudo usermod -aG docker ${USER}"
+   	"sudo systemctl enable docker",
+   	"sudo systemctl start docker",
     ]
-  }
 }
 
 provisioner "file" {
-    source      = "./dockerfiles/grafana/Dockerfile"
+	source      = "./dockerfiles/grafana/Dockerfile"
     destination = "/home/ubuntu/dockerfiles/grafana/Dockerfile"
-  }
+}
 
-  provisioner "remote-exec" {
-    inline = [
-      "docker build -t grafana /home/ubuntu/dockerfiles/grafana",
-      "docker run -d grafana"
-    ]
-  }
+provisioner "file" {
+	source = "./dockerfiles/prometheus/Dockerfile"
+  	destination = "/home/ubuntu/dockefriles/prometheus/Dockerfile"
+}
 
-  provisioner "file" {
-
-  source = "./dockerfiles/prometheus/Dockerfile"
-  destination = "/home/ubuntu/dockefriles/prometheus/Dockerfile"
-  }
-
-  provisioner "remote-exec" {
-  inline [
-  "docker build -t prometheus /home/ubuntu/dockerfiles/prometheus
-  "docker run -d prometheus"
+provisioner "remote-exec" {
+	inline [
+  	"docker build -t prometheus /home/ubuntu/dockerfiles/prometheus
+  	"docker run -d prometheus"
   	]
-  }
+}
 
-  output "instance_ip" {
-  value = aws_eip.example.public_ip
+provisioner "remote-exec" {
+	inlune [
+	"docker build -t prometheus /home/ubuntu/dockerfiles/prometheus
+  	"docker run -d prometheus"
+	]
+}
+
+output "instance_ip" {
+	value = aws_eip.terraform-task-eip.public_ip
 }
