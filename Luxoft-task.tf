5b05e818fb05813f35d94c3a7dc37998741f040c
beatplugg <nonameqwerty87@gmail.com>
2023-05-17 15:25:15 +0300

    fixes

--- Luxoft-task.tf
+++ Luxoft-task.tf
@@ -72,7 +72,7 @@ provisioner "remote-exec" {
    	"echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable' | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
    	"sudo apt-get update",    
    	"sudo apt-get install -y docker-ce docker-ce-cli containerd.io",
-   	"sudo usermod -aG docker ${USER}"
+   	"sudo usermod -aG docker ${USER}",
    	"sudo systemctl enable docker",
    	"sudo systemctl start docker",
     ]
