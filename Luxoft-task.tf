c16e5df850fe84ad2a54ebafa5a71040f7cce1f6
beatplugg <nonameqwerty87@gmail.com>
2023-05-18 14:10:54 +0300

    gitignore and user_data provisioning

--- Luxoft-task.tf
+++ Luxoft-task.tf
@@ -84,9 +84,9 @@ resource "aws_instance" "terraform-task-instance" {
   	tee /etc/apt/sources.list.d/docker.list > /dev/null
   	apt-get update
   	apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-  	sudo usermod -aG docker $USER
-	sudo systemctl enable docker
-	sudo systemctl start docker
+  	usermod -aG docker $USER
+	systemctl enable docker
+	systemctl start docker
 	cd /home/ubuntu/docker-compose/
 	docker-compose up -d
 	EOF
